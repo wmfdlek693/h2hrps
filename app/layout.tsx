939c2688d19fe91553250016c9caba8f53ed08c3
wmfdlek693 <wmfdlek693@naver.com>
2026-07-18 19:50:41 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,16 @@ import "./globals.css";
 export const metadata: Metadata = {
   metadataBase: new URL("https://peppi-h2hrps.vercel.app"),
 
-  title: "Hearts2Hearts RPS 취향표",
+  title: "핱페스 취향표",
   description:
-    "Hearts2Hearts 8명의 방향성 조합 취향을 색으로 표시하고 이미지로 저장하는 팬메이드 도구입니다.",
+    "핱페스 취향표를 자동 생성해주는 도구입니다.",
 
   openGraph: {
-    title: "Hearts2Hearts RPS 취향표",
+    title: "핱페스 취향표",
     description:
-      "Hearts2Hearts 8명의 방향성 조합 취향을 색으로 표시하고 이미지로 저장하는 팬메이드 도구입니다.",
+      "핱페스 취향표를 자동 생성해주는 도구입니다.",
     url: "https://peppi-h2hrps.vercel.app",
-    siteName: "Hearts2Hearts RPS 취향표",
+    siteName: "핱페스 취향표",
     images: [
       {
         url: "/opengraph-image.png",
@@ -29,9 +29,9 @@ export const metadata: Metadata = {
 
   twitter: {
     card: "summary_large_image",
-    title: "Hearts2Hearts RPS 취향표",
+    title: "핱페스 취향표",
     description:
-      "Hearts2Hearts 8명의 방향성 조합 취향을 색으로 표시하고 이미지로 저장하는 팬메이드 도구입니다.",
+      "H핱페스 취향표를 자동 생성해주는 도구입니다.",
     images: ["/twitter-image.png"],
   },
 
